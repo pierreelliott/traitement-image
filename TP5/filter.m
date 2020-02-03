8295d33578eef038c8e8a344bfb06ee088e1b9b3
pierreelliott <pierreelliott.github@thiboud.fr>
2020-02-03 11:49:53 +0100

    Hint for filter

--- TP5/filter.m
+++ TP5/filter.m
@@ -1,6 +1,10 @@
-function filtered = filter (image, filtre, size)
+function filtered = filter (image, filtre, ssize)
   
-  sfiltre = fspecial(filtre, size);
-  filtered = conv2(image, sfiltre, 'same');
+  %sfiltre = fspecial(filtre, size);
+  elt = strel('disk', ssize, 0)
+  resize = size(image) - size(elt)
+  filter = padarray(getnhood(test), image_size, direction="both");
+  % Multiply image by filter
+  %filtered = conv2(image, sfiltre, 'same');
 
 endfunction
